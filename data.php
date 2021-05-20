7836a4c722889af0f47126ef44dbcc3e71ad62dc
Diego_Souza <diego.s.costa@fiesc.com.br>
2021-05-20 16:54:45 -0300

    Adicionando um dia apos dia de hoje 20/05

--- data.php
+++ data.php
@@ -8,4 +8,14 @@ echo $agora . PHP_EOL;
 
 $hoje1 = new DateTime('now');
 
+$maisUmDia = new DateInterval('P1D');
+
+$hoje2 = new DateTimeImmutable();
+
+$amanha = $hoje2->add($maisUmDia);
+
+echo $hoje2->format('d/m') . PHP_EOL;
+echo $amanha->format('d/m') . PHP_EOL;
+
+
 echo $hoje1->format('H:i') . PHP_EOL;
